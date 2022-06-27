250d46174dd52a737afd3de1bd25b54002e7086f
Rennan Sapucaia <rennansapucaia17@gmail.com>
2022-06-27 15:59:02 -0300

    leitor de números
    Pede um requerimento de um número, se o numero tiver dentro dos parâmetros ele dará a confirmação, caso ele não estiver no parâmetro certo, ele ira dar uma outra resposta.

---
+++ variaveis-e-inputs/app.js
@@ -0,0 +1,23 @@
+const { Console } = require('console');
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let leituraDeCampo;
+readline.question('Digite um numero de 1 a 6: ', input => {
+    leituraDeCampo = input;
+    if(leituraDeCampo > 6){
+        console.log("Burro");
+    }
+    else if(leituraDeCampo < 0){
+        console.log("Burro");
+    }
+    else
+    {
+        console.log(`O seu numero é: ${leituraDeCampo}`);
+    }
+
+});
+
+
